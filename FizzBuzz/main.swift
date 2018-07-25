a770f1621e436020b94eee55759b7b63a472e368
Dima Manyahin <dima.manyahin@skylum.com>
2018-07-25 12:55:45 +0300

    Second try

---
+++ FizzBuzz/main.swift
@@ -0,0 +1,21 @@
+//
+//  main.swift
+//  FizzBuzz
+//
+//  Created by Dima Manyahin on 7/25/18.
+//  Copyright © 2018 Dima Manyahin. All rights reserved.
+//
+
+import Foundation
+
+func dividedBy3Description(_ number: Int) -> String {
+    
+}
+
+func dividedBy5Description(_ number: Int) -> String {
+    
+}
+
+func numberDescription(_ number: Int) -> String {
+    
+}
