6b460e4d27a7463cee1b28cbcf6b16564b6ad057
Dima Manyahin <dima.manyahin@skylum.com>
2018-07-25 10:40:45 +0300

    Add initial project

---
+++ SwiftLabs/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftLabs
+//
+//  Created by Dima Manyahin on 7/25/18.
+//  Copyright © 2018 Dima Manyahin. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
